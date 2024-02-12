041d1f3a8cfc7a6f64b475e3ce1cc78d3da28bf6
Alumnouem <Alumnouem@N205-20.alumnos.uem.es>
2024-02-13 00:51:29 +0100

    primer commit rama 1

--- PruebasGit/src/PruebasEGit/PruebasEGit.java
+++ PruebasGit/src/PruebasEGit/PruebasEGit.java
@@ -5,7 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("PruebasEgit");
 		System.out.println("Segundo Commit");
-
+		System.out.println("Primer commit rama 1");
 	}
 
 }
